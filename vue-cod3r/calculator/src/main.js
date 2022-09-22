8581de8969f631ca17da4f72393894dd5360950b
Eliabe-ciandt <eliabe@ciandt.com>
2022-09-22 12:46:08 -0300

    Why the versions changes so much between one and another?

--- vue-cod3r/calculator/src/main.js
+++ vue-cod3r/calculator/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// eslint-disable-next-line no-unused-vars
+import Vue, { createApp } from "vue";
+import App from "./App";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
